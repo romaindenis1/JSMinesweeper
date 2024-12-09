4043f2addf0f9ca5345d5da5dd1b2b12a438e70e
Romain <163823947+romaindenis1@users.noreply.github.com>
2024-12-09 14:04:29 +0100

    add(Grid)
    Just a simple commit to start, mostly just css [WIP]
    Grid is made depending on difficulty.
    The fun begins now :)

---
+++ main.js
@@ -0,0 +1,58 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+
+let gridSize = 10;  //Default Easy
+let cellSize = canvas.width / gridSize;
+
+
+function drawGrid() {
+    //Clear
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //Border
+    ctx.strokeStyle = '#000'; 
+    ctx.lineWidth = 1;
+
+    //Draw
+    for (let i = 0; i <= gridSize; i++) {
+        //Vertical 
+        ctx.beginPath();  
+        ctx.moveTo(i * cellSize, 0);
+        ctx.lineTo(i * cellSize, canvas.height);
+        ctx.stroke(); 
+
+        //Horizontal 
+        ctx.beginPath();  
+        ctx.moveTo(0, i * cellSize);
+        ctx.lineTo(canvas.width, i * cellSize);
+        ctx.stroke(); 
+    }
+}
+
+//Difficulty handler
+document.getElementById('difficulty').addEventListener('change', (event) => {
+    
+    switch (event.target.value) {
+        case 'easy':
+            gridSize = 10; 
+            break;
+        case 'medium':
+            gridSize = 15; 
+            break;
+        case 'hard':
+            gridSize = 20; 
+            break;
+        case 'insane':
+            gridSize = 30; 
+            break;
+    }
+
+    //Change size
+    cellSize = canvas.width / gridSize;
+
+    drawGrid();
+});
+
+
+drawGrid();
